3064695be65bf8f880fa6f43549393f3ea3d12b0
Ramses <141248+R-VdP@users.noreply.github.com>
2022-06-11 16:05:34 +0200

    Move the nvim config into a separate file.

--- hosts/nixer.nix
+++ hosts/nixer.nix
@@ -17,6 +17,8 @@ let
 in
 
 {
+  imports = [ ../nvim.nix ];
+
   environment.systemPackages = with pkgs; [
     stack
     ghc
@@ -24,138 +26,6 @@ in
     nixos-option
   ];
 
-  programs.neovim = {
-    enable = true;
-    configure = {
-      customRC = ''
-        set nocompatible            " disable compatibility to old-time vi
-        set mouse=v                 " middle-click paste with
-        set mouse=a                 " enable mouse click
-
-        colorscheme jellybeans
-
-        set encoding=utf-8
-        set scrolloff=3
-        set backspace=indent,eol,start
-        set undofile
-
-        set list
-        set listchars=tab:▸\ ,eol:¬,trail:·
-
-        set termguicolors
-
-        set hlsearch                " highlight search
-        set incsearch               " incremental search
-        set ignorecase              " case insensitive
-        set smartcase
-        set showmatch               " show matching
-
-        set tabstop=2               " number of columns occupied by a tab
-        set softtabstop=2           " see multiple spaces as tabstops so <BS> does the right thing
-        set expandtab               " converts tabs to white space
-        set shiftwidth=2            " width for autoindents
-        "set autoindent              " indent a new line the same amount as the line just typed
-
-        " remove trailing whitespace
-        autocmd BufWritePre * :%s/\s\+$//e
-
-        set ruler
-        set cursorline
-        set number relativenumber
-        set laststatus=2
-        set cc=80                   " set an 80 column border for good coding style
-        set cmdheight=2             " height of the command window on the bottom
-
-        set wildmenu
-        set wildmode=list:longest   " get bash-like tab completions
-
-        filetype plugin indent on   " allow auto-indenting depending on file type
-        syntax on                   " syntax highlighting
-        set clipboard=unnamedplus   " using system clipboard
-        filetype plugin on
-
-        set ttyfast                 " Speed up scrolling in Vim
-
-        silent !mkdir ~/.cache/vim > /dev/null 2>&1
-        set backupdir=~/.cache/vim " Directory to store backup files.
-
-        set updatetime=150
-
-        autocmd BufReadPost *
-          \ if line("'\"") >= 1 && line("'\"") <= line("$") |
-          \   exe "normal! g`\"" |
-          \ endif
-
-        " Airline
-        let g:airline_theme = 'bubblegum'
-        let g:airline_powerline_fonts = 1
-        let g:airline#extensions#tabline#enabled = 1
-        let g:airline#extensions#tabline#left_sep = ' '
-        let g:airline#extensions#tabline#left_alt_sep = '|'
-
-        " unicode symbols
-        if !exists('g:airline_symbols')
-          let g:airline_symbols = {}
-        endif
-
-        let g:airline_left_sep = '»'
-        "let g:airline_left_sep = '▶'
-        let g:airline_right_sep = '«'
-        "let g:airline_right_sep = '◀'
-        "let g:airline_symbols.linenr = '␊'
-        "let g:airline_symbols.linenr = '␤'
-        "let g:airline_symbols.linenr = '¶'
-        let g:airline_symbols.linenr = '⮃'
-        let g:airline_symbols.colnr = '⮀'
-        let g:airline_symbols.branch = '⎇'
-        let g:airline_symbols.paste = 'ρ'
-        "let g:airline_symbols.paste = 'Þ'
-        "let g:airline_symbols.paste = '∥'
-        let g:airline_symbols.whitespace = 'Ξ'
-
-        " Open files in new tabs
-        let NERDTreeCustomOpenArgs={'file':{'where': 't'}}
-
-        " Keybindings
-        let mapleader = ","
-
-        " F1 opens NERDTree
-        nnoremap <F1> :NERDTreeToggle<CR>
-        " Use double-<space> to save the file
-        nnoremap <space><space> :w<cr>
-        " Remap jj to Esc.
-        inoremap jj <Esc>
-        " Remove search highlighting
-        nnoremap <leader><space> :noh<cr>
-        " Tab jumps to matching bracket
-        nnoremap <tab> %
-        " Tab jumps to matching bracket
-        vnoremap <tab> %
-
-        luafile ${../nvim.lua}
-      '';
-      packages.nix = with pkgs.vimPlugins; {
-        start = [
-          (nvim-treesitter.withPlugins (plugins: pkgs.tree-sitter.allGrammars))
-          vim-nix
-          haskell-vim
-          dracula-vim
-          vim-airline
-          vim-airline-themes
-          vim-colorschemes
-          indentLine
-          nerdtree
-          nvim-lspconfig
-          nvim-cmp
-          cmp-nvim-lsp
-        ];
-        opt = [];
-      };
-    };
-    withRuby    = false;
-    withPython3 = false;
-    withNodeJs  = false;
-  };
 
   time.timeZone = "Europe/Brussels";
 
