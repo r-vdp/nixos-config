a7619e4bdcc850aa0e05279357c41ce5f3015b83
Ramses <141248+R-VdP@users.noreply.github.com>
2022-10-08 16:15:54 +0200

    Add standard XDG dirs to env.

--- org.nix
+++ org.nix
@@ -22,6 +22,14 @@ let
 in
 {
   config = {
+
+    environment.sessionVariables = rec {
+      XDG_CACHE_HOME = "\${HOME}/.cache";
+      XDG_CONFIG_HOME = "\${HOME}/.config";
+      XDG_DATA_HOME = "\${HOME}/.local/share";
+      XDG_STATE_HOME = "\${HOME}/.local/state";
+    };
+
     system.activationScripts.tunnel_key_permissions = mkForce "";
 
     system.autoUpgrade = {
