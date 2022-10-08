84a3352c570bb0c4a0c59be2ff8efb283330ae49
Ramses <141248+R-VdP@users.noreply.github.com>
2022-10-08 17:20:16 +0200

    No need for rec.

--- org.nix
+++ org.nix
@@ -23,7 +23,7 @@ in
 {
   config = {
 
-    environment.sessionVariables = rec {
+    environment.sessionVariables = {
       XDG_CACHE_HOME = "\${HOME}/.cache";
       XDG_CONFIG_HOME = "\${HOME}/.config";
       XDG_DATA_HOME = "\${HOME}/.local/share";
