54e0c2c72db0c4101a182bc5e9c66423558f5fe8
R-VdP <141248+R-VdP@users.noreply.github.com>
2022-11-19 16:11:29 +0200

    Fix permissions on ~/.config/sops

--- users/ramses/home.nix
+++ users/ramses/home.nix
@@ -49,16 +49,18 @@ in
       # Automatically put in place the age key corresponding to our SSH key
       ssh-to-age =
         let
-          out_dir = "${config.home.homeDirectory}/.config/sops/age/";
-          out_file = "${out_dir}/keys.txt";
+          sops_dir = "${config.xdg.configHome}/sops/";
+          age_dir = "${sops_dir}/age/";
+          out_file = "${age_dir}/keys.txt";
         in
         dag.entryAfter [ "writeBoundary" ] ''
           ''${DRY_RUN_CMD} rm --force ''${VERBOSE_ARG} ${out_file}
-          ''${DRY_RUN_CMD} mkdir --parents ''${VERBOSE_ARG} ${out_dir}
+          ''${DRY_RUN_CMD} mkdir --parents ''${VERBOSE_ARG} ${age_dir}
           ''${DRY_RUN_CMD} ${pkgs.ssh-to-age}/bin/ssh-to-age \
             -private-key \
             -i ${privKeyFile} \
             -o ${out_file}
+          ''${DRY_RUN_CMD} chmod --recursive u=rwX,g=,o= ''${VERBOSE_ARG} ${sops_dir}
         '';
     };
   };
