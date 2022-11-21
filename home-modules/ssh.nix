dbae69b313e3523709019138644cde470d206fe1
R-VdP <141248+R-VdP@users.noreply.github.com>
2022-11-21 14:17:56 +0200

    Simplify SSH config.

--- home-modules/ssh.nix
+++ home-modules/ssh.nix
@@ -23,73 +23,82 @@ in
       AddKeysToAgent = no
       Ciphers = aes256-gcm@openssh.com,chacha20-poly1305@openssh.com
     '';
-    matchBlocks = {
-      nixer = dag.entryBefore [ "tmux" ] {
-        host = "nixer nixer-tmux";
-        hostname = "sshv6.engyandramses.xyz";
-        port = 2443;
-      };
-      nixer-local = dag.entryBefore [ "tmux" ] {
-        host = "nixer-local nixer-local-tmux";
-        hostname = "nixer.local";
-      };
-      nixer-relayed = dag.entryBefore [ "tmux" ] {
-        host = "nixer-relayed nixer-relayed-tmux";
-        hostname = "localhost";
-        port = 6012;
-        proxyJump = "ssh-relay-proxy";
-      };
-      rescue-iso = dag.entryBefore [ "tmux" ] {
-        host = "rescue-iso rescue-iso-tmux";
-        hostname = "localhost";
-        port = 8000;
-        proxyJump = "ssh-relay-proxy";
-        extraOptions = {
-          UserKnownHostsFile = "/dev/null";
-          GlobalKnownHostsFile = "/dev/null";
-          StrictHostKeyChecking = "no";
+    matchBlocks =
+      let
+        needs_tmux = dag.entryBefore [ "tmux" ];
+      in
+      {
+        nixer = needs_tmux {
+          host = "nixer nixer-tmux";
+          hostname = "sshv6.engyandramses.xyz";
+          port = 2443;
+        };
+        nixer-local = needs_tmux {
+          host = "nixer-local nixer-local-tmux";
+          hostname = "nixer.local";
+        };
+        sshrelay2 = needs_tmux {
+          host = "sshrelay2 sshrelay2-tmux";
+          hostname = "sshrelay2.ocb.msf.org";
+          port = 443;
+        };
+        ssh-relay-proxy = {
+          host = "ssh-relay-proxy";
+          hostname = "sshrelay.ocb.msf.org";
+          user = "tunneller";
+          port = 443;
+        };
+        nixer-relayed = needs_tmux {
+          host = "nixer-relayed nixer-relayed-tmux";
+          hostname = "localhost";
+          port = 6012;
+          proxyJump = "ssh-relay-proxy";
+        };
+        rescue-iso = needs_tmux {
+          host = "rescue-iso rescue-iso-tmux";
+          hostname = "localhost";
+          port = 8000;
+          proxyJump = "ssh-relay-proxy";
+          extraOptions = {
+            UserKnownHostsFile = "/dev/null";
+            GlobalKnownHostsFile = "/dev/null";
+            StrictHostKeyChecking = "no";
+          };
+        };
+        generic = needs_tmux {
+          host = "generic generic-tmux";
+          hostname = "localhost";
+          proxyJump = "ssh-relay-proxy";
+        };
+        proxy = {
+          host = "proxy";
+          hostname = "sshrelay.ocb.msf.org";
+          port = 443;
+          dynamicForwards = [
+            { port = 9443; }
+          ];
+          extraOptions = {
+            ExitOnForwardFailure = "yes";
+            RequestTTY = "false";
+            SessionType = "none";
+            PermitLocalCommand = "yes";
+            LocalCommand = ''echo "Started tunnel at $(date)..."'';
+          };
+        };
+        github = {
+          host = "github.com";
+          hostname = "ssh.github.com";
+          user = "git";
+          port = 443;
+        };
+        tmux = {
+          host = "*-tmux";
+          extraOptions = {
+            RequestTTY = "Force";
+            RemoteCommand = "tmux attach";
+          };
         };
       };
-      ssh-relay-proxy = {
-        host = "ssh-relay-proxy";
-        hostname = "sshrelay.ocb.msf.org";
-        user = "tunneller";
-        port = 443;
-      };
-      generic = dag.entryBefore [ "tmux" ] {
-        host = "generic generic-tmux";
-        hostname = "localhost";
-        proxyJump = "ssh-relay-proxy";
-      };
-      proxy = {
-        host = "proxy";
-        hostname = "sshrelay.ocb.msf.org";
-        port = 443;
-        dynamicForwards = [
-          { port = 9443; }
-        ];
-        extraOptions = {
-          ExitOnForwardFailure = "yes";
-          RequestTTY = "false";
-          SessionType = "none";
-          PermitLocalCommand = "yes";
-          LocalCommand = ''echo "Started tunnel at $(date)..."'';
-        };
-      };
-      github = {
-        host = "github.com";
-        hostname = "ssh.github.com";
-        user = "git";
-        port = 443;
-      };
-      tmux = {
-        host = "*-tmux";
-        extraOptions = {
-          RequestTTY = "Force";
-          RemoteCommand = "tmux attach";
-        };
-      };
-    };
   };
 }
 
