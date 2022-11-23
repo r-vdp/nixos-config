3dfc5cd045e078db12132aa125f00387b8269ce0
R-VdP <141248+R-VdP@users.noreply.github.com>
2022-11-23 13:38:54 +0200

    Initialise the nix profile when needed.

--- home-modules/nix-index.nix
+++ home-modules/nix-index.nix
@@ -13,5 +13,18 @@ in
       nix-index-database.legacyPackages.${pkgs.system}.database;
   };
 
-  programs.nix-index.enable = true;
+  programs = {
+    nix-index.enable = true;
+
+    bash.initExtra = ''
+      # Create an empty intial profile
+      # Some tools, like NixOS' command-not-found, check for
+      # ~/.nix-profile/manifest.json to decide whether we use the nix3 interface.
+      if [[ ! -e "''${HOME}/.nix-profile/manifest.json" ]]; then
+        nix profile install "nixpkgs#hello"
+        nix profile remove 0
+        nix profile wipe-history
+      fi
+    '';
+  };
 }
