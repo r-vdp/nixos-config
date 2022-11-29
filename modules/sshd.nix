7762afc643a5dc4e87bdcf15ae93c56a8dc3f42c
R-VdP <141248+R-VdP@users.noreply.github.com>
2022-11-30 00:29:17 +0200

    Enable openssh.

---
+++ modules/sshd.nix
@@ -0,0 +1,41 @@
+{ lib, ... }:
+
+with lib;
+
+{
+  services.openssh = {
+    enable = true;
+
+    # Ignore the authorized_keys files in the users' home directories,
+    # keys should be added through the config.
+    authorizedKeysFiles = mkForce [ "/etc/ssh/authorized_keys.d/%u" ];
+    permitRootLogin = "no";
+    forwardX11 = false;
+    passwordAuthentication = false;
+    kbdInteractiveAuthentication = false;
+    allowSFTP = true;
+    kexAlgorithms = [
+      "sntrup761x25519-sha512@openssh.com"
+      "curve25519-sha256@libssh.org"
+    ];
+    ciphers = [
+      "aes256-gcm@openssh.com"
+      "chacha20-poly1305@openssh.com"
+    ];
+    macs = [
+      "hmac-sha2-512-etm@openssh.com"
+      "hmac-sha2-256-etm@openssh.com"
+      "umac-128-etm@openssh.com"
+    ];
+    extraConfig = ''
+      StrictModes yes
+      AllowAgentForwarding no
+      TCPKeepAlive yes
+      ClientAliveInterval 10
+      ClientAliveCountMax 5
+      GSSAPIAuthentication no
+      KerberosAuthentication no
+    '';
+  };
+}
+
