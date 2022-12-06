042100aaba8ee48319c81b71b30acc50e62480c2
R-VdP <141248+R-VdP@users.noreply.github.com>
2022-12-06 17:33:07 +0200

    Include file path in lualine.

--- home-modules/nvim.lua
+++ home-modules/nvim.lua
@@ -191,7 +191,7 @@ require('lualine').setup {
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename', path = 1},
+    lualine_c = {{'filename', path = 1}},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location', num_of_lines}
@@ -199,7 +199,7 @@ require('lualine').setup {
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename', path = 1},
+    lualine_c = {{'filename', path = 1}},
     lualine_x = {},
     lualine_y = {'progress'},
     lualine_z = {'location', num_of_lines}
