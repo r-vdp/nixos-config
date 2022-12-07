5a4ea847a4f7aa3338b3a7e070afbeeceb632b15
R-VdP <141248+R-VdP@users.noreply.github.com>
2022-12-07 14:37:49 +0200

    Use nixpkgs-unfree to build the NixOS configs.

--- flake.nix
+++ flake.nix
@@ -1,13 +1,22 @@
 {
   nixConfig = {
-    extra-trusted-public-keys =
-      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-    extra-substituters = "https://devenv.cachix.org";
+    extra-substituters = [
+      "https://nixpkgs-unfree.cachix.org"
+      "https://devenv.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+    ];
   };
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unfree = {
+      url = "github:numtide/nixpkgs-unfree";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs = {
@@ -17,7 +26,11 @@
     };
     sops-nix = {
       url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        # Avoid a plethora of nixpkgs-stable entries in flake.lock by syncing them.
+        nixpkgs-stable.follows = "devenv/pre-commit-hooks/nixpkgs-stable";
+      };
     };
     devenv = {
       url = "github:cachix/devenv";
@@ -30,12 +43,11 @@
     { self
     , flake-utils
     , nixpkgs
+    , nixpkgs-unfree
     , home-manager
     , sops-nix
     , ...
-    }@inputs: with nixpkgs.lib;
-
-    {
+    }@inputs: with nixpkgs.lib; {
       nixosModules.default = {
         imports = [
           ./modules
@@ -49,7 +61,7 @@
         let
           system = flake-utils.lib.system.x86_64-linux;
           mkStandardHost = hostname:
-            nixpkgs.lib.nixosSystem {
+            nixpkgs-unfree.lib.nixosSystem {
               specialArgs = { inherit inputs; };
               modules = [
                 self.nixosModules.default
