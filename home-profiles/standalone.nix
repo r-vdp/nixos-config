9e3dfb2ea3019b722cd9c3f183472253f99673ff
R-VdP <141248+R-VdP@users.noreply.github.com>
2022-12-08 23:35:23 +0100

    Configure nix and nixpkgs for standalone instances.

--- home-profiles/standalone.nix
+++ home-profiles/standalone.nix
@@ -3,6 +3,20 @@
   home = {
     inherit (inputs) username;
     homeDirectory = "/home/${config.home.username}";
+    # FIXME: we are repeating modules/nix.nix here
+    nix.settings = {
+      auto-optimise-store = true;
+      experimental-features = [ "nix-command" "flakes" ];
+      trusted-users = [ "root" "@wheel" ];
+      fallback = true;
+      warn-dirty = false;
+      connect-timeout = 5;
+      log-lines = 50;
+
+      min-free = 128 * 1000 * 1000; # 128 MB
+      max-free = 1000 * 1000 * 1000; # 1 GB
+    };
+    nixpkgs.config.allowUnfree = true;
     settings = {
       isHeadless = true;
       keys.privateKeyFiles =
