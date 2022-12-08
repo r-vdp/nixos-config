76c21ae8572b0321f19082209df2c6266abe5b3d
R-VdP <141248+R-VdP@users.noreply.github.com>
2022-12-08 23:29:50 +0100

    Do no create the age private key on standalone instances.

--- home-modules/ssh-to-age.nix
+++ home-modules/ssh-to-age.nix
@@ -4,21 +4,30 @@ let
   inherit (lib.hm) dag;
 in
 {
-  # Automatically put in place the age key corresponding to our SSH key
-  home.activation.ssh-to-age =
-    let
-      sops_dir = "${config.xdg.configHome}/sops/";
-      age_dir = "${sops_dir}/age/";
-      out_file = "${age_dir}/keys.txt";
-    in
-    dag.entryAfter [ "writeBoundary" ] ''
-      ''${DRY_RUN_CMD} rm --force ''${VERBOSE_ARG} ${out_file}
-      ''${DRY_RUN_CMD} mkdir --parents ''${VERBOSE_ARG} ${age_dir}
-      ''${DRY_RUN_CMD} ${pkgs.ssh-to-age}/bin/ssh-to-age \
-        -private-key \
-        -i ${config.home.settings.keys.privateKeyFiles.current} \
-        -o ${out_file}
-      ''${DRY_RUN_CMD} chmod --recursive u=rwX,g=,o= ''${VERBOSE_ARG} ${sops_dir}
-    '';
+  options.home.settings.ssh-to-age.enable =
+    lib.mkEnableOption "the ssh-to-age activation snippet";
+
+  config = lib.mkIf config.home.settings.ssh-to-age.enable {
+    # Automatically put in place the age key corresponding to our SSH key
+    home.activation.ssh-to-age =
+      let
+        sops_dir = "${config.xdg.configHome}/sops/";
+        age_dir = "${sops_dir}/age/";
+        out_file = "${age_dir}/keys.txt";
+
+        private_key_file = config.home.settings.keys.privateKeyFiles.current;
+      in
+      dag.entryAfter [ "writeBoundary" ] ''
+        ''${DRY_RUN_CMD} rm --force ''${VERBOSE_ARG} ${out_file}
+        if [[ -f "${private_key_file}" ]]; then
+          ''${DRY_RUN_CMD} mkdir --parents ''${VERBOSE_ARG} ${age_dir}
+          ''${DRY_RUN_CMD} ${pkgs.ssh-to-age}/bin/ssh-to-age \
+            -private-key \
+            -i ${private_key_file} \
+            -o ${out_file}
+          ''${DRY_RUN_CMD} chmod --recursive u=rwX,g=,o= ''${VERBOSE_ARG} ${sops_dir}
+        fi
+      '';
+  };
 }
 
--- home-profiles/integrated.nix
+++ home-profiles/integrated.nix
@@ -7,20 +7,26 @@ in
 {
   home = {
     inherit (osConfig.system) stateVersion;
+
     homeDirectory =
       lib.mkDefault osConfig.users.users.${config.home.username}.home;
+
     settings = {
       isHeadless = osConfig.settings.system.isHeadless;
-      tmux_term = osConfig.settings.system.tmux_term;
+
       keys = {
         privateKeyFiles = {
           id_ec = secrets."${config.home.username}-ssh-priv-key".path;
           current = secrets."${config.home.username}-2-ssh-priv-key".path;
         };
       };
+      ssh-to-age.enable = true;
+
       extraSshConfig = {
         extra-ssh-config = osConfig.sops.secrets."ssh-extra-config".path;
       };
+
+      tmux_term = osConfig.settings.system.tmux_term;
     };
   };
 }
