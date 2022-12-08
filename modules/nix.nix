7376050908ef08b9e4e416cb114a3fb30dba16d6
R-VdP <141248+R-VdP@users.noreply.github.com>
2022-12-08 22:40:58 +0100

    Set nix.conf values.
    See https://jackson.dev/post/nix-reasonable-defaults/

--- modules/nix.nix
+++ modules/nix.nix
@@ -5,10 +5,18 @@ let
 in
 {
   nix = {
+    # man nix.conf
     settings = {
       auto-optimise-store = true;
       experimental-features = [ "nix-command" "flakes" ];
       trusted-users = [ "root" "@wheel" ];
+      fallback = true;
+      warn-dirty = false;
+      connect-timeout = 5;
+      log-lines = 50;
+
+      min-free = 128 * 1000 * 1000; # 128 MB
+      max-free = 1000 * 1000 * 1000; # 1 GB
     };
     gc = {
       automatic = true;
