efa420a26feebd042853bc681cfd26ca729e1ca5
R-VdP <141248+R-VdP@users.noreply.github.com>
2022-12-09 13:36:18 +0100

    Fix HM standalone profile nix settings.

--- home-profiles/standalone.nix
+++ home-profiles/standalone.nix
@@ -3,20 +3,6 @@
   home = {
     inherit (inputs) username;
     homeDirectory = "/home/${config.home.username}";
-    # FIXME: we are repeating modules/nix.nix here
-    nix.settings = {
-      auto-optimise-store = true;
-      experimental-features = [ "nix-command" "flakes" ];
-      trusted-users = [ "root" "@wheel" ];
-      fallback = true;
-      warn-dirty = false;
-      connect-timeout = 5;
-      log-lines = 50;
-
-      min-free = 128 * 1000 * 1000; # 128 MB
-      max-free = 1000 * 1000 * 1000; # 1 GB
-    };
-    nixpkgs.config.allowUnfree = true;
     settings = {
       isHeadless = true;
       keys.privateKeyFiles =
@@ -30,5 +16,18 @@
     };
     stateVersion = "22.05";
   };
+
+  # FIXME: we are repeating modules/nix.nix here
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    fallback = true;
+    warn-dirty = false;
+    connect-timeout = 5;
+    log-lines = 50;
+
+    min-free = 128 * 1000 * 1000; # 128 MB
+    max-free = 1000 * 1000 * 1000; # 1 GB
+  };
+  nixpkgs.config.allowUnfree = true;
 }
 
