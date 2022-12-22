0627051350ae2711e74eb0988e87847a985f84a3
R-VdP <141248+R-VdP@users.noreply.github.com>
2022-12-22 23:52:26 +0100

    HM activation script runs with set -u

--- home-modules/update-diff.nix
+++ home-modules/update-diff.nix
@@ -1,11 +1,12 @@
 { pkgs, ... }:
 
 {
+  # The HM activate script runs with "set -u", and so errors on unbound variables.
+  # We need to check with "-v" if a variable exists before we can use it.
   home.activation.diff = ''
-    if [[ -n "$oldGenPath" && -n "$newGenPath" ]]; then
+    if [[ -v "oldGenPath" && -v "newGenPath" ]]; then
       echo "Home manager closure diff:"
       $DRY_RUN_CMD ${pkgs.nix}/bin/nix store diff-closures "$oldGenPath" "$newGenPath"
     fi
   '';
 }
-
