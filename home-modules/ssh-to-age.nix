9238c6ed79a299f8994106a37ffc3ae5392a9765
R-VdP <141248+R-VdP@users.noreply.github.com>
2023-01-12 18:10:22 +0100

    Store the generated AGE key under /run.

--- home-modules/ssh-to-age.nix
+++ home-modules/ssh-to-age.nix
@@ -13,21 +13,27 @@ in
       let
         sops_dir = "${config.xdg.configHome}/sops/";
         age_dir = "${sops_dir}/age/";
-        out_file = "${age_dir}/keys.txt";
 
         private_key_file = config.home.settings.keys.privateKeyFiles.current;
       in
       dag.entryAfter [ "writeBoundary" ] ''
-        ''${DRY_RUN_CMD} rm --force ''${VERBOSE_ARG} ${out_file}
+        if [[ -L "${age_dir}" ]]; then
+          ''${DRY_RUN_CMD} unlink ''${VERBOSE_ARG} "${age_dir}"
+        else
+          ''${DRY_RUN_CMD} rm --force --recursive ''${VERBOSE_ARG} "${age_dir}"
+        fi
         if [[ -f "${private_key_file}" ]]; then
-          ''${DRY_RUN_CMD} mkdir --parents ''${VERBOSE_ARG} ${age_dir}
+          real_dir="''${XDG_RUNTIME_DIR}/age"
+          real_file="''${real_dir}/keys.txt"
+          ''${DRY_RUN_CMD} mkdir --parents ''${VERBOSE_ARG} "${sops_dir}"
+          ''${DRY_RUN_CMD} mkdir --parents ''${VERBOSE_ARG} "''${real_dir}"
           ''${DRY_RUN_CMD} ${pkgs.ssh-to-age}/bin/ssh-to-age \
             -private-key \
             -i ${private_key_file} \
-            -o ${out_file}
-          ''${DRY_RUN_CMD} chmod --recursive u=rwX,g=,o= ''${VERBOSE_ARG} ${sops_dir}
+            -o ''${real_file}
+          ''${DRY_RUN_CMD} ln --symbolic ''${VERBOSE_ARG} ''${real_dir} "${age_dir}"
+          ''${DRY_RUN_CMD} chmod --recursive u=rwX,g=,o= ''${VERBOSE_ARG} "${sops_dir}"
         fi
       '';
   };
 }
-
