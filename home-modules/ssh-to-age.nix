d23ad50902aef8375a407a2f9c7a7a237b6feedf
R-VdP <141248+R-VdP@users.noreply.github.com>
2023-01-17 13:16:29 +0100

    Run ssh-to-age as a systemd user service.
    Before this change we were running this from the
    home-manager activation script, but on NixOS this
    script runs in the environment of the systemd system
    instance which means that variables like
    XDG_RUNTIME_DIR are not available.
    It is also not needed to have this secret available
    when the user is not logged in.

--- home-modules/ssh-to-age.nix
+++ home-modules/ssh-to-age.nix
@@ -1,39 +1,68 @@
 { config, lib, pkgs, ... }:
 
-let
-  inherit (lib.hm) dag;
-in
 {
   options.home.settings.ssh-to-age.enable =
     lib.mkEnableOption "the ssh-to-age activation snippet";
 
   config = lib.mkIf config.home.settings.ssh-to-age.enable {
-    # Automatically put in place the age key corresponding to our SSH key
-    home.activation.ssh-to-age =
-      let
-        sops_dir = "${config.xdg.configHome}/sops/";
-        age_dir = "${sops_dir}/age/";
+    systemd.user = {
+      startServices = "sd-switch";
 
-        private_key_file = config.home.settings.keys.privateKeyFiles.current;
-      in
-      dag.entryAfter [ "writeBoundary" ] ''
-        if [[ -L "${age_dir}" ]]; then
-          ''${DRY_RUN_CMD} unlink ''${VERBOSE_ARG} "${age_dir}"
-        else
-          ''${DRY_RUN_CMD} rm --force --recursive ''${VERBOSE_ARG} "${age_dir}"
-        fi
-        if [[ -f "${private_key_file}" ]]; then
-          real_dir="''${XDG_RUNTIME_DIR}/age"
-          real_file="''${real_dir}/keys.txt"
-          ''${DRY_RUN_CMD} mkdir --parents ''${VERBOSE_ARG} "${sops_dir}"
-          ''${DRY_RUN_CMD} mkdir --parents ''${VERBOSE_ARG} "''${real_dir}"
-          ''${DRY_RUN_CMD} ${pkgs.ssh-to-age}/bin/ssh-to-age \
-            -private-key \
-            -i ${private_key_file} \
-            -o ''${real_file}
-          ''${DRY_RUN_CMD} ln --symbolic ''${VERBOSE_ARG} ''${real_dir} "${age_dir}"
-          ''${DRY_RUN_CMD} chmod --recursive u=rwX,g=,o= ''${VERBOSE_ARG} "${sops_dir}"
-        fi
-      '';
+      services =
+        let
+          mkScriptString = name: content:
+            toString (pkgs.writeShellScript name content);
+        in
+        {
+          ssh-to-age =
+            let
+              sops_dir = "${config.xdg.configHome}/sops";
+              age_dir = "${sops_dir}/age";
+              private_key_file = config.home.settings.keys.privateKeyFiles.current;
+
+              clean = ''
+                if [[ -L "${age_dir}" ]]; then
+                  ${pkgs.coreutils}/bin/unlink "${age_dir}"
+                else
+                  ${pkgs.coreutils}/bin/rm --force --recursive "${age_dir}"
+                fi
+              '';
+            in
+            {
+              Unit = {
+                Description = ''
+                  Automatically put in place the age key corresponding to our SSH key
+                '';
+              };
+
+              Service =
+                {
+                  Type = "oneshot";
+                  RemainAfterExit = true;
+                  ExecStart = mkScriptString "ssh-to-age-start" ''
+                    ${clean}
+                    if [[ -f "${private_key_file}" ]]; then
+                      real_dir="''${XDG_RUNTIME_DIR}/age"
+                      real_file="''${real_dir}/keys.txt"
+                      ${pkgs.coreutils}/bin/mkdir --parents "${sops_dir}"
+                      ${pkgs.coreutils}/bin/mkdir --parents "''${real_dir}"
+                      ${pkgs.ssh-to-age}/bin/ssh-to-age \
+                        -private-key \
+                        -i ${private_key_file} \
+                        -o ''${real_file}
+                      ${pkgs.coreutils}/bin/ln --symbolic "''${real_dir}" "${age_dir}"
+                      ${pkgs.coreutils}/bin/chmod --recursive u=rwX,g=,o= "${sops_dir}"
+                    fi
+                  '';
+
+                  ExecStop = mkScriptString "ssh-to-age-stop" clean;
+                };
+
+              Install = {
+                WantedBy = [ "default.target" ];
+              };
+            };
+        };
+    };
   };
 }
