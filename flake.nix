bffa38e37ca960accafad46d521c969eee6364a5
R-VdP <141248+R-VdP@users.noreply.github.com>
2023-01-22 13:58:20 +0100

    Add the nix-community binary cache.

--- flake.nix
+++ flake.nix
@@ -2,9 +2,11 @@
   nixConfig = {
     extra-substituters = [
       "https://devenv.cachix.org"
+      "https://nix-community.cachix.org"
     ];
     extra-trusted-public-keys = [
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 
