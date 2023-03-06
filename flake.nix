72ba37df20ad14db1cb5fe73c94479482256793e
R-VdP <141248+R-VdP@users.noreply.github.com>
2023-03-06 11:03:35 +0100

    Limit our flake to linux for now.

--- flake.nix
+++ flake.nix
@@ -94,88 +94,94 @@
         ];
     }
     //
-    flake-utils.lib.eachDefaultSystem (system: {
-      packages =
-        let
-          pkgs = inputs.nixpkgs.legacyPackages.${system};
-        in
-        {
-          coreboot-configurator = pkgs.libsForQt5.callPackage ./pkgs/coreboot-configurator.nix { };
-
-          flashrom = pkgs.flashrom.overrideAttrs (prevAttrs: {
-            src = pkgs.fetchzip {
-              url = "https://download.flashrom.org/releases/flashrom-v${prevAttrs.version}.tar.bz2";
-              hash = "sha256-rXwD8kpIrmmGJQu0NHHjIPGTa4+xx+H0FdqwAwo6ePg=";
-            };
-
-            nativeBuildInputs = prevAttrs.nativeBuildInputs ++ (with pkgs; [
-              meson
-              ninja
-            ]);
-
-            buildInputs = prevAttrs.buildInputs ++ (with pkgs; [
-              cmocka
-            ]);
-
-            mesonFlags = [
-              "-Dprogrammer=auto"
-            ];
-
-            postInstall =
-              let
-                udevRulesPath = "lib/udev/rules.d/flashrom.rules";
-              in
-              ''
-                # After the meson build, the udev rules file is no longer present
-                # in the build dir, so we need to get it from $src and patch it
-                # again.
-                # There might be a better way to do this...
-                install -Dm644 $src/util/flashrom_udev.rules $out/${udevRulesPath}
-                substituteInPlace $out/${udevRulesPath} --replace 'GROUP="plugdev"' 'TAG+="uaccess", TAG+="udev-acl"'
-              '';
-          });
-
-          # Build with "nix build '.#rescue-iso'
-          rescue-iso = nixos-generators.nixosGenerate {
-            inherit system;
-            specialArgs = { inherit inputs; };
-            modules = [
-              self.nixosModules.default
-              ./hosts/rescue-iso
-            ];
-            format = "iso";
-          };
-        };
-
-      legacyPackages = {
-        # As a temporary workaround, we put the home configurations under
-        # legacyPackages so that we do not need to hard code the value for system.
-        # See https://github.com/nix-community/home-manager/issues/3075
-        homeConfigurations =
+    flake-utils.lib.eachSystem
+      [
+        flake-utils.lib.system.x86_64-linux
+        flake-utils.lib.system.aarch64-linux
+      ]
+      (system: {
+        packages =
           let
-            mkHomeConfig = username: hostname:
-              home-manager.lib.homeManagerConfiguration {
-                pkgs = nixpkgs.legacyPackages.${system};
-                extraSpecialArgs = { inherit inputs; };
-                modules = [
-                  ./hosts/${hostname}
-                  ./home-modules
-                  ./home-profiles/standalone.nix
-                  ./users/ramses/home.nix
-                  { home = { inherit username; }; }
-                ];
+            pkgs = inputs.nixpkgs.legacyPackages.${system};
+          in
+          {
+            coreboot-configurator = pkgs.libsForQt5.callPackage ./pkgs/coreboot-configurator.nix { };
+
+            flashrom = pkgs.flashrom.overrideAttrs (prevAttrs: {
+              src = pkgs.fetchzip {
+                url = "https://download.flashrom.org/releases/flashrom-v${prevAttrs.version}.tar.bz2";
+                hash = "sha256-rXwD8kpIrmmGJQu0NHHjIPGTa4+xx+H0FdqwAwo6ePg=";
               };
 
-            mkHomeConfigEntry = { username, hostname }:
-              nameValuePair "${username}@${hostname}"
-                (mkHomeConfig username hostname);
-          in
-          listToAttrs (
-            map mkHomeConfigEntry [
-              { username = "ramses"; hostname = "dev1"; }
-              { username = "ramses"; hostname = "generic"; }
-            ]
-          );
-      };
-    });
+              nativeBuildInputs = prevAttrs.nativeBuildInputs ++ (with pkgs; [
+                meson
+                ninja
+              ]);
+
+              buildInputs = prevAttrs.buildInputs ++ (with pkgs; [
+                cmocka
+              ]);
+
+              mesonFlags = [
+                "-Dprogrammer=auto"
+              ];
+
+              postInstall =
+                let
+                  udevRulesPath = "lib/udev/rules.d/flashrom.rules";
+                in
+                ''
+                  # After the meson build, the udev rules file is no longer present
+                  # in the build dir, so we need to get it from $src and patch it
+                  # again.
+                  # There might be a better way to do this...
+                  install -Dm644 $src/util/flashrom_udev.rules $out/${udevRulesPath}
+                  substituteInPlace $out/${udevRulesPath} \
+                    --replace 'GROUP="plugdev"' 'TAG+="uaccess", TAG+="udev-acl"'
+                '';
+            });
+
+            # Build with "nix build '.#rescue-iso'
+            rescue-iso = nixos-generators.nixosGenerate {
+              inherit system;
+              specialArgs = { inherit inputs; };
+              modules = [
+                self.nixosModules.default
+                ./hosts/rescue-iso
+              ];
+              format = "iso";
+            };
+          };
+
+        legacyPackages = {
+          # As a temporary workaround, we put the home configurations under
+          # legacyPackages so that we do not need to hard code the value for system.
+          # See https://github.com/nix-community/home-manager/issues/3075
+          homeConfigurations =
+            let
+              mkHomeConfig = username: hostname:
+                home-manager.lib.homeManagerConfiguration {
+                  pkgs = nixpkgs.legacyPackages.${system};
+                  extraSpecialArgs = { inherit inputs; };
+                  modules = [
+                    ./hosts/${hostname}
+                    ./home-modules
+                    ./home-profiles/standalone.nix
+                    ./users/ramses/home.nix
+                    { home = { inherit username; }; }
+                  ];
+                };
+
+              mkHomeConfigEntry = { username, hostname }:
+                nameValuePair "${username}@${hostname}"
+                  (mkHomeConfig username hostname);
+            in
+            listToAttrs (
+              map mkHomeConfigEntry [
+                { username = "ramses"; hostname = "dev1"; }
+                { username = "ramses"; hostname = "generic"; }
+              ]
+            );
+        };
+      });
 }
